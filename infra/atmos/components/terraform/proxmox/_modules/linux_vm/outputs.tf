517295f25f5d627c04f1c5d82aef6dd2be5f7b09
rodrizapico <rodri.zapico@gmail.com>
2026-07-14 00:46:20 +0000

    feat: add ssh connection information to linux_vm outputs

--- infra/atmos/components/terraform/proxmox/_modules/linux_vm/outputs.tf
+++ infra/atmos/components/terraform/proxmox/_modules/linux_vm/outputs.tf
@@ -12,3 +12,18 @@ output "ipv4_address" {
   description = "The deployed VM's ipv4 address"
   value       = try(proxmox_virtual_environment_vm.linux_vm.ipv4_addresses[1][0], null)
 }
+
+output "ssh_user" {
+  description = "The SSH username"
+  value       = local.cloud_init.username
+}
+
+output "generated_ssh_public_key" {
+  description = "The SSH public key, if generated"
+  value       = try(tls_private_key.ssh_key[0].public_key_openssh, null)
+}
+
+output "generated_ssh_private_key" {
+  description = "The SSH private key, if generated"
+  value       = try(tls_private_key.ssh_key[0].private_key_openssh, null)
+}
